64aa04561651c6a30cd011ff84b7b9a91765f2d1
Tony Duran <yoet92@gmail.com>
2019-07-28 03:17:43 -0300

    se agrego el cobtrolador de errores

--- AjaxUtil.js
+++ AjaxUtil.js
@@ -2,9 +2,13 @@
 
 //AJAXUTIL
 var AjaxUtil = function(settings){
-    var http  = new XMLHttpRequest();
-    var settings = settings || {};
-    if (settings.type.toUpperCase() == "GET"){
+    var http            = new XMLHttpRequest();
+    var settings        = settings || {};
+    settings.type       = settings.type || "POST";
+    settings.data       = settings.data || {};
+    settings.success    = settings.success || new Function();
+    settings.error      = settings.error || new Function();
+    if (settings.type.toUpperCase() === "GET"){
         http.onreadystatechange = function(){
             if (this.readyState == 4 && this.status == 200){
                 settings.success(JSON.parse(http.responseText));
@@ -16,19 +20,24 @@ var AjaxUtil = function(settings){
         http.open('GET', settings.url, true);
         http.send();
     }
-    if(settings.type.toUpperCase() == "POST"){
-            var params = new FormData();
-            Object.keys(settings.data).forEach(function(valor, index){
-                params.append(valor, settings.data[valor])
-            })
-            http.open('POST', settings.url, true);
-            http.onload = function () {
-                settings.success(JSON.parse(http.responseText));
-            };
-            http.onerror = function(){
-                settings.error(http.responseText)
-            }
-            http.send(params);
+    if (settings.type.toUpperCase() === "POST"){
+        var params = new FormData();
+        Object.keys(settings.data).forEach(function(valor, index){
+            params.append(valor, settings.data[valor])
+        })
+        http.open('POST', settings.url, true);
+        http.onload = function () {
+            settings.success(JSON.parse(http.responseText));
+        };
+        http.onerror = function(){
+            settings.error(http.responseText)
+        }
+        http.send(params);
+        }
+    if (settings.type.toUpperCase() !== "POST" && settings.type.toUpperCase() !== "GET"){
+        console.error("Propiedad Type Invalida");
+        var err = new Error("El Tipo del Metodo debe ser POST o GET")
+        settings.error(err);
         }
     }
 
@@ -39,7 +48,7 @@ var AjaxUtil = function(settings){
     //         console.log("BIEN", data)
     //     },
     //     error: function(err){
-    //         console.log("ERROR")
+    //         console.log("ERROR", err)
     //     }
     // })
     // AjaxUtil({
